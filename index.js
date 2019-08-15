73522ab923f004f2de7b390913effc664ae6dd18
Frankie Ye <frankie9793@gmail.com>
2019-08-15 23:16:30 +0800

    fix app not loading

--- index.js
+++ index.js
@@ -6,16 +6,6 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 
-
-// Static single page app can't be served with dynamic routes FIX
-if(process.env.NODE_ENV === 'production'){
-  //set static folder
-  app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-
 //View engine setup 
 
 app.engine('handlebars', exphbs());
@@ -69,6 +59,15 @@ app.post('/send', (req, res) => {
       });
     
       
+});
+
+// Static single page app can't be served with dynamic routes FIX
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.js'));
 });
 
 
