a077ad89896bc267c24580d6d8a24dd25029c11f
Frankie Ye <frankie9793@gmail.com>
2019-08-16 08:43:04 +0800

    console logging

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.get('/', (req, res) => {
 
 app.post('/send', (req, res) => {
     console.log(req.body);
+    console.log(process.env.from);
+    
     const output = `
         <p>You have a new contact request</p>
         <h3>Contact Details</h3>
